42113b1375b8b361a905815c86b8045c79ccf153
ziplok2 <sergejkocerzuk75@gmail.com>
2024-05-25 08:54:31 +0300

    Изменения

--- Repozitoriy/Icomics.cs
+++ Repozitoriy/Icomics.cs
@@ -5,7 +5,7 @@ namespace Comics.Repozitoriy
     public interface Icomics
     {
         public Comics.Model.Comics Add(Comics.Model.Comics comics);
-        public Comics.Model.Comics Get (int ID);
+        public Comics.Model.Comics GetID (int ID);
         public Comics.Model.Comics Update(Comics.Model.Comics comics);
         public List <Comics.Model.Comics> GetAll ();
         public Comics.Model.Comics Delete (int ID);
